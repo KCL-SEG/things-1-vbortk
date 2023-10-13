9dfb991ffc62309c6df282a51b65fa88ccf51eb5
Vytas <vboloki@gmail.com>
2023-10-13 14:50:15 +0100

    Add a basic things model

--- things/models.py
+++ things/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.db.models import Model
 
-# Create your models here.
+class Thing(Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=120)
+    quantity = models.IntegerField()
